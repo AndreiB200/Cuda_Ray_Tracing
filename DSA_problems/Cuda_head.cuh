8f95582cfe8bb52a1411896de26aae79f4e030b6
Andrei Burlacu <andreiburlacu2000@gmail.com>
2023-12-12 15:21:06 +0200

    Delete DSA_problems directory

--- DSA_problems/Cuda_head.cuh
+++
@@ -1,8 +0,0 @@
-#include <glad/glad.h>
-
-#include <cuda_runtime.h>
-#include <cuda_gl_interop.h>
-#include <device_launch_parameters.h>
-
-void init_Cuda(unsigned int opengl_buffer);
-void launch_kernel();
